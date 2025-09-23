d708f52c225031e9592612d91e3c3ff3a6eca88f
AurelienL <aurelienlefebvre10@gmail.com>
2025-09-23 17:18:14 +0200

    add route to index & cart

--- routes/cart.js
+++ routes/cart.js
@@ -2,24 +2,29 @@ var express = require("express");
 var router = express.Router();
 const { checkBody } = require("../modules/checkBody");
 const Booking = require("../models/bookings");
-const Trip = require("../models/trips");
+const Cart = require("../models/carts");
 
 // GET /cart
 router.get("/", async function (req, res) {
   try {
-    const trips = await Trip.find();
+    const trips = await Cart.find();
     console.log(trips);
     res.json({ result: true });
   } catch (error) {
-    return res.json({ result: false, error: "Something wrong happened while purchasing." });
+    return res.json({
+      result: false,
+      error: "Something wrong happened while purchasing.",
+    });
   }
 });
 
 // POST /cart when user want to purchase trips
-router.post("/", async function (req, res) {
+router.post("/purchase", async function (req, res) {
   const { trips } = req.body;
   if (!checkBody(req.body, ["trips"])) {
-    return res.status(500).json({ result: false, error: "Missing or empty fields" });
+    return res
+      .status(500)
+      .json({ result: false, error: "Missing or empty fields" });
   }
 
   try {
@@ -32,7 +37,34 @@ router.post("/", async function (req, res) {
     await newBooking.save();
     res.json({ result: true, message: "Purchase successfully done." });
   } catch (error) {
-    return res.json({ result: false, error: "Something wrong happened while purchasing." });
+    return res.json({
+      result: false,
+      error: "Something wrong happened while purchasing.",
+    });
+  }
+});
+
+// DELETE cart ONE TRIP
+router.delete("/", async function (req, res) {
+  const { trip } = req.body;
+  if (!checkBody(req.body, ["trip"])) {
+    return res
+      .status(500)
+      .json({ result: false, error: "Missing or empty fields" });
+  }
+
+  try {
+    const carts = await Cart.find();
+    const cart = carts[0];
+    const filterCart = cart.trips.filter((item) => item._id !== trip._id);
+    await Cart.updateOne({ _id: cart._id }, { trips: filterCart });
+
+    res.json({ result: true, message: "Delete cart OK" });
+  } catch (error) {
+    return res.json({
+      result: false,
+      error: "Something wrong happened while deleting.",
+    });
   }
 });
 
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ var express = require("express");
 var router = express.Router();
 const Trip = require("../models/trips");
 const { checkBody } = require("../modules/checkBody");
+const Cart = require("../models/carts");
 
 /* POST user search. */
-router.post("/", (req, res) => {
+router.post("/search", (req, res) => {
   const { departure, arrival, date } = req.body;
   try {
     if (checkBody(req.body, ["departure", "arrival", "date"])) {
@@ -16,6 +17,32 @@ router.post("/", (req, res) => {
         res.json({ trips: data, result: true });
       });
     } else res.json({ result: false, error: "Missing or empty fields" });
+  } catch (error) {
+    return res.json({
+      result: false,
+      error: "Something wrong happened while getting trips.",
+    });
+  }
+});
+
+/* POST ADD TO CART. */
+
+router.post("/addtocart", (req, res) => {
+  const { trip } = req.body;
+  try {
+    if (checkBody(req.body, ["trip"])) {
+      Cart.find({}).then((data) => {
+        const tripsCart = data[0].trips;
+        tripsCart.push(trip);
+        Cart.updateOne({ _id: data[0]._id }, { trips: tripsCart }).then(
+          (data) => {
+            res.json({ result: true, message: "Add to cart OK" });
+          }
+        );
+      });
+    } else {
+      res.json({ result: false, error: "Missing or empty fields" });
+    }
   } catch (error) {
     return res.json({
       result: false,
